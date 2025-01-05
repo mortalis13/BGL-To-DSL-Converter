0f284ec017e290df8edab428dce55b4df81c6e5c
Ilya Kremer <ilya@kremer.pro>
2025-01-05 16:22:27 +0100

    [T-1] Reopen the file after reading for the metadata extraction; improve formatting

--- r.bat
+++ r.bat
@@ -10,7 +10,6 @@ if exist %app_name%.exe (
   del %app_name%.exe
 )
 
-
 if "%1"=="c" (
   make clean
   goto:eof
@@ -20,12 +19,12 @@ echo == Building App ==
 make
 
 if not "%1"=="b" (
-if exist %app_name%.exe (
-  echo.
-  echo == Calling App ==
-  echo '%app_name%.exe %dict_path%'
-  echo.
+  if exist %app_name%.exe (
+    echo.
+    echo == Calling App ==
+    echo '%app_name%.exe %dict_path%'
+    echo.
 
-  %app_name%.exe "%dict_path%"
-)
+    %app_name%.exe "%dict_path%"
+  )
 )
